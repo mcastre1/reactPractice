28200b2bd687f6d8dd613026007c2c9493407228
mcastre1 <miguelcastrejongal@gmail.com>
2023-12-11 20:10:02 -0700

    Fixed formating

--- src/App.tsx
+++ src/App.tsx
@@ -34,35 +34,42 @@ function App() {
           </div>
         </div>
         <div className="container-fluid w-100 h-60">
-          <div className="row">
+          <div className="row row-eq-height">
             <div className="col-sm">
-              <div className="border rounded bg-light h-50 m-2">
-                <ThreeLinkContent
-                  header="Learn about Dynamics 365 Sales"
-                  first_url="#"
-                  first_desc="Get Started with Sales"
-                  second_url="#"
-                  second_desc="Set up your business process"
-                  third_url="#"
-                  third_desc="Customize with Power Apps"
-                ></ThreeLinkContent>
+              <div className="h-50" style={{ paddingBottom: 5 }}>
+                <div
+                  className="border rounded bg-light h-100"
+                  style={{ marginBottom: 10 }}
+                >
+                  <ThreeLinkContent
+                    header="Learn about Dynamics 365 Sales"
+                    first_url="mailto: abc@example.com?subject = Feedback&body = Message"
+                    first_desc="Get Started with Sales"
+                    second_url="#"
+                    second_desc="Set up your business process"
+                    third_url="#"
+                    third_desc="Customize with Power Apps"
+                  ></ThreeLinkContent>
+                </div>
               </div>
-              <div className=" border rounded bg-light h-50 m-2">
-                <ThreeLinkContent
-                  header="Learn about Power Apps"
-                  first_url="#"
-                  first_desc="Model Driven Apps"
-                  second_url="#"
-                  second_desc="Canvas Apps"
-                  third_url="#"
-                  third_desc="Dataverse"
-                ></ThreeLinkContent>
+              <div className="h-50" style={{ paddingTop: 5 }}>
+                <div className=" border rounded bg-light h-100">
+                  <ThreeLinkContent
+                    header="Learn about Power Apps"
+                    first_url="#"
+                    first_desc="Model Driven Apps"
+                    second_url="#"
+                    second_desc="Canvas Apps"
+                    third_url="#"
+                    third_desc="Dataverse"
+                  ></ThreeLinkContent>
+                </div>
               </div>
             </div>
-            <div className="col-sm border rounded bg-light h-100 m-2">
+            <div className="col-sm border rounded bg-light h-100">
               <VideoDesc
                 header="Learn about JourneyTeam"
-                desc="We've crafted this app in Dynamics 365 to simplify sales, like a seasoned cowboy making a tough job look easy, to boost productivity and keep customers satisfied."
+                desc=""
                 url="https://www.youtube.com/embed/SqcY0GlETPk?si=hZ0gXGWJ5RiMUoKn"
               ></VideoDesc>
             </div>
